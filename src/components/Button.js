6434d554205fb62077a9e9c43bbe07fd8a867194
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-08-23 11:43:26 -0500

    add button w listener

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Button = ({ color, text }) => {
+    return (
+        <button style={{backgroundColor: color}} className='btn'>{text}</button>
+    )
+}
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 // using ES7 snippets extension, rafce
 import React from 'react'
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({title}) => {
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            <Button color='green' text='Add' />
         </header>
     )
 }
@@ -17,5 +19,11 @@ Header.defaultProps = {
 Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'red', 
+//     backgroundColor: 'black'
+// }
 
 export default Header
