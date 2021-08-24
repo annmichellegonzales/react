edaeb13e25d3372f392d33b145436c9ed5023e89
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-08-24 13:49:07 -0500

    begin toggle

--- src/App.js
+++ src/App.js
@@ -24,16 +24,23 @@ function App() {
 }
 ])
 
-// Delete task
+// Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
+
+// Toggle Reminder
+const toggleReminder = (id) => {
+  console.log(id)
+
+}
+
 
   return (
     <div className='container'>
       <Header />
       {tasks.length > 0 ? (
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         'No Tasks to Show'
       )}
