e7c716e970d97ba73d9619ff7a926703c32c5b16
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-08-25 08:50:02 -0500

    toggle add button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from "./components/Tasks";
 import AddTask from './components/AddTask';
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState ([
     {
         id: 1,
@@ -25,6 +26,13 @@ function App() {
 }
 ])
 
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() + 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
+
 // Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
@@ -40,8 +48,8 @@ const toggleReminder = (id) => {
 
   return (
     <div className='container'>
-      <Header />
-      <AddTask />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} />
+      {showAddTask &&  <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
       <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
